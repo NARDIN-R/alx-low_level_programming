533bb3a62a41e9365d9a8ff3c29dc81babd9f461
NARDIN-R <nardinrafat2002@gmail.com>
2023-09-07 14:16:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description: A C program that prints with puts function
  * Return: always 0 (success)
 */
-init main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
