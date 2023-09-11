d4cec7850cd69ff0100a6a7a4d0deeea21892896
NARDIN-R <nardinrafat2002@gmail.com>
2023-09-11 16:28:21 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,7 @@
  *
  * Description: print all alphabet letters
  *
- * Return: 0 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
@@ -20,3 +20,4 @@ int main(void)
 	putchar('\n')
 
 	return (0);
+}
